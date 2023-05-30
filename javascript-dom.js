f1f704364c593888aef142afa4b95ce48c7ec6ce
Andrei Seandru <dunkradu1234@gmail.com>
2023-05-30 14:59:18 +0300

    Understanding DOM Loading Events

---
+++ javascript-dom.js
@@ -0,0 +1,13 @@
+import '../assets/css/style.css';
+
+
+const app = document.getElementById('app');
+app.innerHTML = `
+  <h1>JavaScript DOM</h1>
+`;
+
+document.addEventListener('DOMContentLoaded', () => {
+  alert('DOMContentLoaded');
+});
+
+
