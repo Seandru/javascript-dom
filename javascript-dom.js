481d160409114283134f8edfce3269fde722a585
Andrei Seandru <dunkradu1234@gmail.com>
2023-05-31 17:20:19 +0300

    set & get inline style

--- javascript-dom.js
+++ javascript-dom.js
@@ -3,36 +3,19 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <ul id="list">
-    <li>Earth</li>
-    <li>Fire</li>
-    <li>Water</li>
-    <li>Air</li>
-  </ul>
-`;
-
-import '../assets/css/style.css';
-
-const app = document.getElementById('app');
-app.innerHTML = `
-  <h1>JavaScript DOM</h1>
   <button type="button">
-    Close Me
+    Click Me!
   </button>
-  <input type="text">
 `;
 
-const input = document.querySelector('input');
-//input.value = 2;
-input.setAttribute('value', 2);
-console.log(input.value);
-console.dir(input);
-
+//<button style="padding: 25px; margin: 10px 0;">
 const button = document.querySelector('button');
-//set
-button.setAttribute('aria-label', 'Close this Modal');
-//get
-const value = button.getAttribute('aria-label');
-console.log(value);
-//.attributes
-console.log(button.attributes['aria-label']);
+
+//CSS
+button.style.cssText = 'padding: 25px; margin: 10px 0; font-size: 20px;';
+
+//direct property access
+button.style.fontSize = '22px';
+button.style.marginTop = '15px';
+
+console.log(button.style.fontSize);
