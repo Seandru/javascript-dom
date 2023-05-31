00dcd97b01a9a30e7d7134e3729f43b66bde4ead
Andrei Seandru <dunkradu1234@gmail.com>
2023-05-31 11:39:08 +0300

    Querying DOM Nodes (HTMLCollections)

--- javascript-dom.js
+++ javascript-dom.js
@@ -1,12 +1,39 @@
-import './assets/css/style.css';
+import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
+  <ul id="list"></ul>
 `;
 
-const div = document.createElement('div');
-div.innerText = 'I am a message!';
+const data = ['Earth', 'Fire', 'Water'];
+const fragment = document.createDocumentFragment();
 
-app.append(div);
-setTimeout(() => div.remove(), 2500);
+data.forEach(item => {
+  const li = document.createElement('li');
+  li.className = 'list-item';
+  li.innerText = item;
+  fragment.append(li);
+});
+
+// getElementById: HTMLElement
+const ulFromId = document.getElementById('list');
+console.log(ulFromId);
+ulFromId.append(fragment);
+
+// getElementsByClassName: HTMLCollection
+const listItemsFromClassName = ulFromId.getElementsByClassName('list-item');
+console.log(listItemsFromClassName);
+
+// getElementsByTagName
+const listItemsFromTagName = ulFromId.getElementsByTagName('li');
+console.log(listItemsFromTagName);
+
+//Live collection
+const newListItem = document.createElement('li');
+newListItem.className = 'list-item';
+newListItem.innerText = 'Air';
+ulFromId.append(newListItem);
+
+console.log(listItemsFromClassName);
+console.log(listItemsFromTagName);
