e2b2d481df06680e8ae781e2d9ed2fde5c5cc9d1
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-01 12:41:34 +0300

    Transforming FormData for the Server

--- javascript-dom.js
+++ javascript-dom.js
@@ -16,6 +16,25 @@ app.innerHTML = `
       </select>  
     </label>
     </label>
+    <div>
+      What size?
+      <label>
+        Small
+        <input type="radio" name="size" value="small" checked>
+      </label>
+      <label>
+        Medium
+        <input type="radio" name="size" value="medium">
+      </label>
+      <label>
+        Large
+        <input type="radio" name="size" value="large">
+      </label>
+    </div>
+    <label>
+        Quantity
+        <input type="number" name="quantity" value="1">
+      </label>
     <button>
       Submit
     </button>
@@ -26,7 +45,18 @@ const form = document.forms.order;
 
 function handleSubmit(event){
   event.preventDefault();
-  console.log(new FormData(event.target));
+  //query string - fullname=FirstName+SecondName&pizza=pepperoni&size=large&quantity=2
+  //content-type = application/x-www-form-urlencoded
+  const formData = new FormData(event.target);
+  // const data = [...formData.entries()];
+  // const asString = data.map(x => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`).join('&');
+  // console.log(asString);
+  const asString = new URLSearchParams(formData).toString();
+  console.log(asString);
+
+  //json
+  const asJSON = JSON.stringify(Object.fromEntries(formData));
+  console.log(asJSON);
 }
 
 function handleFormData(event){
