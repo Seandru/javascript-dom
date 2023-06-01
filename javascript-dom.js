3f311fbd79f4f28e62b7466d104916288b844d34
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-01 14:04:35 +0300

    Handling Checkbox Input Elements

--- javascript-dom.js
+++ javascript-dom.js
@@ -4,48 +4,28 @@ const app = document.getElementById('app');
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
   <form name="example">
-  <div class="container">
     <label>
-    Blue
-      <input type="radio" name="color" value="blue" checked>
+      Accept Marketing
+      <input type="checkbox" name="marketing">
     </label>
-    <label>
-    Red
-      <input type="radio" name="color" value="red">
-    </label>
-    <label>
-    Green
-      <input type="radio" name="color" value="green">
-    </label>
-  </div>
   </form>
 `;
 
 const form = document.forms.example;
-const radios = [...form.elements.color];
+const checkbox = form.elements.marketing;
 
 //1. Useful properties
-radios.forEach(radio => {
-  console.log(radio.value);
-  console.log(radio.checked);
-})
+console.dir(checkbox);
+//set
+checkbox.checked = true;
+//get
+console.log(checkbox.checked);
 
 //2. Events
-const container = form.querySelector('.container');
-
-container.addEventListener('change', () => {
-  // console.log('Change...');
-  // const checked = radios.find(radio => radio.checked).value;
-  // console.log(checked);
-  console.log(form.elements.color.value);
+checkbox.addEventListener('change', () => {
+  console.log(checkbox.checked);
+  console.log(checkbox.value);
 });
 
-
-
-
-
-
-
-
-
-
+//3. Methods
+checkbox.select();
