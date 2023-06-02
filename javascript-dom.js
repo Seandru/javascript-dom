2a4dd41c673dc90eab1b8ca7c14e89691aee699a
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-02 12:05:42 +0300

    Created a Todo app

--- javascript-dom.js
+++ javascript-dom.js
@@ -2,43 +2,163 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = `
-  <h1>JavaScript DOM</h1>
-  <form name="example">
-    <select name="drink">
-      <option value="">Select Your Drink...</option>
-      <option value="lemonade" selected>Lemonade</option>
-      <option value="cola">Cola</option>
-      <option value="water">Water</option>
-    </select>
-  </form>
+  <div class="todos">
+    <div class="todos-header">
+      <h3 class="todos-title">Todo List</h3>
+        <div>
+          <p>You have <span class="todos-count"></span> items</p>
+          <button type="button" class="todos-clear" style="display: none;">
+            Clear Completed
+          </button>
+        </div>
+    </div>
+    <form class="todos-form" name="todos">
+      <input type="text" placeholder="What's next?" name="todo">
+    </form>
+    <ul class="todos-list"></ul>
+    </div>
 `;
+// state
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-const form = document.forms.example;
-const select = form.elements.drink;
+// selectors
+const root = document.querySelector('.todos');
+const list = root.querySelector('.todos-list');
+const count = root.querySelector('.todos-count');
+const clear = root.querySelector('.todos-clear');
+const form = document.forms.todos;
+const input = form.elements.todo;
 
+// functions
+function saveToStorage(todos){
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
 
-//1. Selected value
-select.value = 'water';
-console.log(select.value);
+function renderTodos(todos) {
+  let todoString = '';
+  todos.forEach((todo, index) => {
+    todoString += `
+      <li data-id="${index}"${todo.complete ? ' class="todos-complete"' : ''}>
+        <input type="checkbox"${todo.complete ? ' checked' : ''}>
+          <span>${todo.label}</span>
+        <button type="button"></button>
+      </li>
+    `;
+  });
+  list.innerHTML = todoString;
+  count.innerText = todos.filter(todo => !todo.complete).length;
+  clear.style.display = todos.filter(todo => todo.complete).length ? 'block' : 'none';
+}
 
-//2. Selected index
-const id = 2;
-select.selectedIndex = id;
-console.dir(select.selectedIndex);
+function addTodo(event) {
+  event.preventDefault();
+  const label = input.value.trim();
+  const complete = false;
+  todos = [
+    ...todos,
+    {
+      label,
+      complete,
+    },
+  ];
+  renderTodos(todos);
+  saveToStorage(todos);
+  input.value = '';
+}
 
-//3. Selected DOM Element
-console.log(select.options[select.selectedIndex]);
+function updateTodo(event){
+  const id = parseInt(event.target.parentNode.getAttribute('data-id'), 10);
+  const complete = event.target.checked;
+  todos = todos.map((todo, index) => {
+    if(index === id){
+      return {
+        ...todo,
+        complete,
+      };
+    }
+    return todo;
+  });
+  renderTodos(todos);
+  saveToStorage(todos);
+}
 
-//4. Events
-select.addEventListener('change', () => {
-  console.log(select.value);
-  console.log(select.selectedIndex);
-  console.log(select.options[select.selectedIndex]);
-})
+function editTodo(event){
+  if(event.target.nodeName.toLowerCase() !== 'span'){
+    return;
+  }
+  const id = parseInt(event.target.parentNode.getAttribute('data-id'), 10);
+  const todoLabel = todos[id].label;
 
-//5. Add new <option>
-const option = document.createElement('option');
-option.value = 'milk';
-option.text = 'Milk';
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.value = todoLabel;
+  
+  function handleEdit(event){
+    event.stopPropagation();
+    const label = this.value;
+    if(label !== todoLabel){
+      todos = todos.map((todo, index) => {
+        if(index === id){
+          return{
+            ...todo,
+            label,
+          };
+        }
+        return todo;
+      });
+      renderTodos(todos);
+      saveToStorage(todos);
+    }
+    //clean up
+    event.target.style.display = '';
+    this.removeEventListener('change', handleEdit);
+    this.remove();
+  }
 
-select.add(option, 1);
+  event.target.style.display = 'none';
+  event.target.parentNode.append(input);
+  input.addEventListener('change', handleEdit);
+  input.focus();
+}
+
+function deleteTodo(event){
+  if(event.target.nodeName.toLowerCase() !== 'button'){
+    return;
+  }
+  const id = parseInt(event.target.parentNode.getAttribute('data-id'), 10);
+  const label = event.target.previousElementSibling.innerText;
+  if(window.confirm(`Delete ${label}?`)){
+    todos = todos.filter((todo, index) => index !== id);
+    renderTodos(todos);
+    saveToStorage(todos);
+  }
+}
+
+function clearCompleteTodos(){
+  const count = todos.filter(todo => todo.complete).length;
+  if(count === 0){
+    return;
+  }
+  if(window.confirm(`Delete ${count} todos?`)){
+    todos = todos.filter(todo => !todo.complete);
+    renderTodos(todos);
+    saveToStorage(todos);
+  }
+}
+
+// init
+function init() {
+  renderTodos(todos);
+  // Add Todo
+  form.addEventListener('submit', addTodo);
+  // update Todo
+  list.addEventListener('change', updateTodo);
+  //edit todo
+  list.addEventListener('dblclick', editTodo);
+  // delete todo
+  list.addEventListener('click', deleteTodo);
+  // complete all todos
+  clear.addEventListener('click', clearCompleteTodos);
+}
+
+init();
